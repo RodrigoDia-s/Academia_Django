ec7397310c27419c7302d265fd82ec81dcc3f9f7
RodrigoDia-s <68355765+RodrigoDia-s@users.noreply.github.com>
2021-12-04 18:22:34 -0300

    Mudanças templates

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, cadastroc
 
+from . import views
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path('cadastro/', cadastroc),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,10 @@
 from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound    
+from django.views.decorators.http import require_http_methods    
 
 class HomePageView(TemplateView):
-    template_name = "teste.html"
+    template_name = "index.html"
+    
+def cadastroc(request):
+    return render(request, "cadastroCliente.html")
