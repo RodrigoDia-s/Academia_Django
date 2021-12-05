34ae02e4bb41c1bad566cf00cdba0cd38424a170
RodrigoDia-s <68355765+RodrigoDia-s@users.noreply.github.com>
2021-12-05 00:47:47 -0300

    planos adicionados

--- paginas/views.py
+++ paginas/views.py
@@ -1,10 +1,19 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound    
-from django.views.decorators.http import require_http_methods    
+from django.views.decorators.http import require_http_methods
+
+from planos.models import Plano    
 
 class HomePageView(TemplateView):
     template_name = "index.html"
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['plano_list'] = Plano.objects.all()
+        return context
+    
     
 def cadastroc(request):
     return render(request, "cadastroCliente.html")
